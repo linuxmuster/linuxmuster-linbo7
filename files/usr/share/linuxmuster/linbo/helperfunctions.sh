df6d92cfdabff0e3f105ab67d79811bfde15dea9
Thomas Schmitt <thomas@linuxmuster.net>
2021-04-26 16:53:11 +0200

    first ctorrent implementation

--- files/usr/share/linuxmuster/linbo/helperfunctions.sh
+++ files/usr/share/linuxmuster/linbo/helperfunctions.sh
@@ -2,7 +2,7 @@
 # helperfunctions for linbo scripts
 #
 # thomas@linuxmuster.net
-# 20200527
+# 20210423
 #
 
 # converting string to lower chars
@@ -172,18 +172,9 @@ check_torrent() {
  local image="$1"
  local torrent="$image.torrent"
  cd "$LINBODIR"
- # in case of directory creation of torrent file will be forced
- [ -d "$image" ] && return 1
  [ -s "$image" ] || return 1
  [ -s "$torrent" ] || return 1
- local tmpfile=/var/tmp/check_torrent.$$
- btshowmetainfo "$torrent" > $tmpfile || return 1
- local filename="$(grep ^"file name" $tmpfile | awk '{ print $3 }')"
- local filesize="$(grep ^"file size" $tmpfile | awk '{ print $3 }')"
- rm $tmpfile
- [ "$filename" = "$(basename $image)" ] || return 1
- local imagesize="$(ls -l $image | awk '{ print $5 }')"
- [ "$filesize" = "$imagesize" ] || return 1
+ ctorrent -c "$torrent" || return 1
  return 0
 }
 
--- files/usr/share/linuxmuster/linbo/linbo-torrenthelper.sh
+++ files/usr/share/linuxmuster/linbo/linbo-torrenthelper.sh
@@ -2,16 +2,16 @@
 #
 # thomas@linuxmuster.net
 # GPL v3
-# 04.12.2009
+# 20210423
 #
 # linbo bittorrent helper script, started in a screen session by init script
 #
 
-options="$@"
+torrent="$1"
+options="-e 100000 $torrent"
 
 while true; do
- /usr/bin/btdownloadcurses $options ; RC="$?"
- # hash check only on initial start
- options="$(echo $options | sed -e 's|--check_hashes 1|--check_hashes 0|')"
- [ "$RC" = "0" ] || exit "$RC"
+ /usr/bin/ctorrent $options || exit 1
+ # hash check only on initial start, add -f parameter
+ echo "$options" | grep -q ^"-f " || options="-f $options"
 done
