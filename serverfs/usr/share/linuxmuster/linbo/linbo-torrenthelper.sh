be7ab225e6ea234179514fe5960919b8c660fe90
Thomas Schmitt <thomas@linuxmuster.net>
2021-05-14 11:18:01 +0200

    further linbo-torrent implementation.

--- serverfs/usr/share/linuxmuster/linbo/linbo-torrenthelper.sh
+++ serverfs/usr/share/linuxmuster/linbo/linbo-torrenthelper.sh
@@ -2,12 +2,13 @@
 #
 # thomas@linuxmuster.net
 # GPL v3
-# 20210507
+# 20210513
 #
 # linbo ctorrent helper script, started in a screen session by init script
 #
 
 torrent="$1"
+[ -s "$torrent" ] || exit 1
 options="-e 100000 $torrent"
 
 while true; do
