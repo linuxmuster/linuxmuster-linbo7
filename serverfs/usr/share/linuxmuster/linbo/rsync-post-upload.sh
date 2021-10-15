42bec422d89afef8a0847edfe6db97413005fbb8
Thomas Schmitt <thomas@linuxmuster.net>
2021-10-15 14:39:10 +0000

    implemented server side of qdiff image upload.

--- serverfs/usr/share/linuxmuster/linbo/rsync-post-upload.sh
+++ serverfs/usr/share/linuxmuster/linbo/rsync-post-upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # thomas@linuxmuster.net
-# 20220114
+# 20220327
 #
 
 # read in linuxmuster specific environment
@@ -48,7 +48,7 @@ if [ -s "$BACKUP" ]; then
   if [ "$RSYNC_EXIT_STATUS" = "0" ]; then
     echo "Upload of $BASENAME was successful." >&2
     case "$EXT" in
-      *.qcow2)
+      *.qcow2|*.qdiff)
         # qcow2 is the first file of image upload, so place backup file in temporary dir
         # cause without info file we don't know the timestamp
         mkdir -p "$BAKTMP"
@@ -59,6 +59,9 @@ if [ -s "$BACKUP" ]; then
         for i in macct reg postsync prestart; do
           cp -f "$IMGDIR"/*."$i" "$BAKTMP" &> /dev/null
         done
+        # move differential image away if qcow2 image was uploaded
+        case "$EXT" in *.qcow2) mv -f "$IMGDIR"/*.qdiff "$BAKTMP" &> /dev/null;; esac
+        # repair permissions of certain file types
         chmod 600 "$BAKTMP"/*.macct &> /dev/null
         ;;
       *)
@@ -95,7 +98,7 @@ fi
 # do something depending on file type
 case "$EXT" in
 
-  *.qcow2)
+  *.qcow2|*.qdiff)
     # restart multicast service if image file was uploaded.
     echo "Image file $BASENAME detected. Restarting multicast service if enabled." >&2
     /etc/init.d/linbo-multicast restart >&2
