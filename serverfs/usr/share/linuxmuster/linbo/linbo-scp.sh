7738e8bf7c1c8d1f57d57b7e66960cf91288b1c3
Thomas Schmitt <thomas@linuxmuster.net>
2022-12-12 19:20:54 +0000

    Fix #86: add --skip-compress parameter to rsync commands to avoid compression of already compressed files.

--- serverfs/usr/share/linuxmuster/linbo/linbo-scp.sh
+++ serverfs/usr/share/linuxmuster/linbo/linbo-scp.sh
@@ -3,11 +3,11 @@
 # linbo scp wrapper
 #
 # thomas@linuxmuster.net
-# 20160916
+# 20221212
 # GPL V3
 #
 
-rsync -e linbo-ssh $@ ; RC="$?"
+RSYNC_SKIP_COMPRESS="/7z/arc/arj/bz2/cab/cloop/deb/gz/gpg/iso/jar/jp2/jpg/jpeg/lz/lz4/lzma/lzo/png/qcow2/qdiff/qt/rar/rzip/s7z/sfx/svgz/tbz/tgz/tlz/txz/xz/z/zip/zst"
+rsync --skip-compress="RSYNC_SKIP_COMPRESS$" -e linbo-ssh $@ ; RC="$?"
 
 exit "$RC"
-
