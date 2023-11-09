0ad09de861d7bb44318da0cc38a4c62ca41c6546
Thomas Schmitt <thomas@linuxmuster.net>
2023-11-09 19:51:57 +0000

    add custom kernel config file.

---
+++ serverfs/etc/linuxmuster/linbo/custom_kernel.ex
@@ -0,0 +1,7 @@
+# example file for custom linbo kernel, used by update-linbofs
+
+# path to kernel image
+KERNELPATH="/boot/vmlinuz-5.15.0-88-generic"
+
+# path to modules directory
+MODULESPATH="/lib/modules/5.15.0-88-generic"
