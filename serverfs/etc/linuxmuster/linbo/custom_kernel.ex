d4201cb2a3c230bc6857eceab9e98c3e35dae419
Thomas Schmitt <thomas@linuxmuster.net>
2023-11-10 08:05:51 +0000

    update custom kernel config file (#107).

--- serverfs/etc/linuxmuster/linbo/custom_kernel.ex
+++ serverfs/etc/linuxmuster/linbo/custom_kernel.ex
@@ -1,7 +1,15 @@
 # example file for custom linbo kernel, used by update-linbofs
+# to be placed under /etc/linuxmuster/linbo/custom_kernel
+#
+# thomas@linuxmuster.net
+# 20231110
+#
+
+# the example below points to the currently active kernel image
+# and modules used by the server
 
 # path to kernel image
-KERNELPATH="/boot/vmlinuz-5.15.0-88-generic"
+KERNELPATH="/boot/vmlinuz-$(uname -r)"
 
-# path to modules directory
-MODULESPATH="/lib/modules/5.15.0-88-generic"
+# path to the corresponding modules directory
+MODULESPATH="/lib/modules/$(uname -r)"
