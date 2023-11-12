503b18191eecec2f0fc576e10f3583a3a1ac0b0f
Thomas Schmitt <thomas@linuxmuster.net>
2023-11-12 12:50:16 +0000

    add ability to use Linbo's legacy kernel 5.15.138 alternatively.

--- serverfs/etc/linuxmuster/linbo/custom_kernel.ex
+++ serverfs/etc/linuxmuster/linbo/custom_kernel.ex
@@ -2,14 +2,18 @@
 # to be placed under /etc/linuxmuster/linbo/custom_kernel
 #
 # thomas@linuxmuster.net
-# 20231110
+# 20231112
 #
+
+# use Linbo's alternative legacy kernel
+# path to kernel image
+KERNELPATH="/var/lib/linuxmuster/linbo/legacy/linbo64"
+# path to the corresponding modules directory
+MODULESPATH="/var/lib/linuxmuster/linbo/legacy/modules"
 
 # the example below points to the currently active kernel image
 # and modules used by the server
-
 # path to kernel image
 KERNELPATH="/boot/vmlinuz-$(uname -r)"
-
 # path to the corresponding modules directory
 MODULESPATH="/lib/modules/$(uname -r)"
