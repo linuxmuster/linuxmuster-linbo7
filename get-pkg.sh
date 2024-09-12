d31ae1950add2b5ce2840bc169ed3c369e5bf287
Thomas Schmitt <thomas@linuxmuster.net>
2024-09-12 19:34:53 +0200

    update github package upload.

--- get-pkg.sh
+++ get-pkg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# get deb from iggy, needs ubuntu 22.04
+# get deb from martini.schmitt.red, needs ubuntu 22.04
 # thomas@linuxmuster.net
-# 20230316
+# 20240904
 #
 
 # get dependencies
@@ -12,9 +12,9 @@ sudo apt-get -y install dpkg-sig gpg rsync || exit 1
 version="$(head -1 debian/changelog | awk -F\( '{print $2}' | awk -F\) '{print $1}')"
 lname="linuxmuster-linbo7"
 lpkg="${lname}_${version}_all.deb"
-fporig="CF1D06F83EE8518CBA80E88F26CB514FFD0B44B2"
+fporig="CCA1DC5BE0F38B0FFCE5FCC68D158AA6CF53F928"
 
 # download debian package files
 mkdir package
-rsync iggy.linuxmuster.net::linbo/linuxmuster-linbo7_${version}\* package/
+rsync repo.schmitt.red::repo/linuxmuster-linbo7_${version}\* package/
 [ -s "package/$lpkg" ] || exit 1
