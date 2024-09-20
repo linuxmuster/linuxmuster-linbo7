76b363511ae114f966a5796cf344a5ad2e0b6a50
Thomas Schmitt <thomas@linuxmuster.net>
2024-09-20 09:34:14 +0200

    firmware example file update.

--- serverfs/etc/linuxmuster/linbo/firmware.ex
+++ serverfs/etc/linuxmuster/linbo/firmware.ex
@@ -4,14 +4,17 @@
 # Be sure to invoke update-linbofs after you have made changes.
 #
 # thomas@linuxmuster.net
-# 20231016
+# 20240920
 #
 
-# Realtek r8168 ethernet adaptors firmware (whole directory)
+# all firmware files for Realtek ethernet adaptors (content of whole directory will be packed)
 rtl_nic
 
-# Realtek RTL8821AE wifi firmware (single file)
+# all firmware files for Realtek wifi adaptors (content of whole directory will be packed)
+rtlwifi
+
+# Realtek RTL8821AE wifi firmware (single file only)
 rtlwifi/rtl8821aefw.bin
 
-# Intel Wi-Fi 6 AX200 firmware (single file)
+# Intel Wi-Fi 6 AX200 firmware (single file only)
 iwlwifi-cc-a0-77.ucode
